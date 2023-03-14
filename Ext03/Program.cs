3e5b0a61b850964bbcd8b27db92aa50a40bb5f93
Elisha <ulaigve1990@mail.ru>
2023-03-14 23:41:23 +0300

    add Ext

---
+++ Ext03/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int num = ReadInt("Введите число: ");
+
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i * i * i}, ");
+}
+
+int ReadInt(string perfect)
+{
+    Console.Write(perfect);
+    return Convert.ToInt32(Console.ReadLine());
+}
